7e8712e00b450dd62036dc317eb5803a511c6e38
Yiyang <chiangyiyang@gmail.com>
2021-09-15 11:26:55 +0800

    換上自己的Token，並且分開存放

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Your access token can be found at: https://cesium.com/ion/tokens.
 // This is the default access token from your ion account
 
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0NWE5NDFlMi1iYzQ2LTQ2M2ItYTExNy05MzdkMjY0OTQxZmQiLCJpZCI6MTg4NzUsInNjb3BlcyI6WyJsZ24iLCJwciIsImFjdyIsImFzbCIsImFzciIsImFzdyIsImFzcyIsImdjIiwibHIiLCJsdyIsInRyIiwidHciLCJ1c2ciXSwiaWF0IjoxNjMxNjc0MjQ1LCJleHAiOjE2MzIyNzkwNDV9.46ywk0VpSb8LmicKVZWmhYfVB0gpjxCfUIICp89ad4k';
+// Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0NWE5NDFlMi1iYzQ2LTQ2M2ItYTExNy05MzdkMjY0OTQxZmQiLCJpZCI6MTg4NzUsInNjb3BlcyI6WyJsZ24iLCJwciIsImFjdyIsImFzbCIsImFzciIsImFzdyIsImFzcyIsImdjIiwibHIiLCJsdyIsInRyIiwidHciLCJ1c2ciXSwiaWF0IjoxNjMxNjc0MjQ1LCJleHAiOjE2MzIyNzkwNDV9.46ywk0VpSb8LmicKVZWmhYfVB0gpjxCfUIICp89ad4k';
 
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
 const viewer = new Cesium.Viewer('cesiumContainer', {
