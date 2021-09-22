945964daec4dcc303cd7b7ac2895c953bfdf2d08
Yiyang <chiangyiyang@gmail.com>
2021-09-22 16:33:07 +0800

    重新整理程式碼並加上註解

--- funs.js
+++ funs.js
@@ -1,5 +1,12 @@
-function flyToDegrees(longitude, latitude, elevation, heading, pitch) {
-    console.log("Testing");
+// 輸入下列參數：
+//      圖台(viewer)
+//      經度(longitude)
+//      緯度(latitude)
+//      高層(elevation)
+//      方位角(heading)
+//      俯仰角(pitch)
+// 相機飛至指定位置。
+export function flyToDegrees(viewer, longitude, latitude, elevation, heading, pitch) {
     viewer.camera.flyTo({
         destination: Cesium.Cartesian3.fromDegrees(longitude, latitude, elevation),
         orientation: {
--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-// Your access token can be found at: https://cesium.com/ion/tokens.
-// This is the default access token from your ion account
+import './token.js';
+import { flyToDegrees } from './funs.js';
 
-// Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0NWE5NDFlMi1iYzQ2LTQ2M2ItYTExNy05MzdkMjY0OTQxZmQiLCJpZCI6MTg4NzUsInNjb3BlcyI6WyJsZ24iLCJwciIsImFjdyIsImFzbCIsImFzciIsImFzdyIsImFzcyIsImdjIiwibHIiLCJsdyIsInRyIiwidHciLCJ1c2ciXSwiaWF0IjoxNjMxNjc0MjQ1LCJleHAiOjE2MzIyNzkwNDV9.46ywk0VpSb8LmicKVZWmhYfVB0gpjxCfUIICp89ad4k';
-
-// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+// 初始化圖台
 const viewer = new Cesium.Viewer('cesiumContainer', {
     terrainProvider: Cesium.createWorldTerrain()
 });
-// Add Cesium OSM Buildings, a global 3D buildings layer.
-const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());
-// Fly the camera to San Francisco at the given longitude, latitude, and height.
-viewer.camera.flyTo({
-    destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
-    orientation: {
-        heading: Cesium.Math.toRadians(0.0),
-        pitch: Cesium.Math.toRadians(-15.0),
-    }
-});
+
+// 設定按鈕功能
+document.getElementById('btnFly')
+    .addEventListener('click', event => flyToDegrees(viewer, 121.5, 23.5, 10000, 0, -90));
