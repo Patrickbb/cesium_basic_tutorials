24ca09612f0e492a03c0475386a9b60d8356a691
Yiyang <chiangyiyang@gmail.com>
2021-09-22 16:43:46 +0800

    加上Cesium Token

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 import './token.js';
 import { flyToDegrees } from './funs.js';
 
+// 設定Token
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1YjhhOWUxNy1iMDVkLTRjOWMtYjVhZC0wMDM5NGFkODdkOGEiLCJpZCI6MTg4NzUsInNjb3BlcyI6WyJsZ24iLCJwciIsImFjdyIsImFzbCIsImFzciIsImFzdyIsImFzcyIsImdjIiwibHIiLCJsdyIsInRyIiwidHciLCJ1c2ciXSwiaWF0IjoxNjMyMzAwMTc3LCJleHAiOjE2MzI5MDQ5Nzd9.r-Z062ATWqFNKJsi25IqtKttYUGKHlpTuqNAb0L_qnA';
+
 // 初始化圖台
 const viewer = new Cesium.Viewer('cesiumContainer', {
     terrainProvider: Cesium.createWorldTerrain()
